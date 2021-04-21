6741259a0e43500c3cca238a1e4b99571909002e
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-04-21 23:40:18 +0200

    nitial compiling version of qtsingleapplication.

---
+++ Libs/qtsingleapplication/files.cmake
@@ -0,0 +1,15 @@
+set(KIT_MOC_SRCS
+  qtlocalpeer.h
+  qtsingleapplication.h
+  qtsinglecoreapplication.h
+)
+
+set(KIT_SRCS
+  qthandlenewappinstance.h
+  qtlockedfile.h
+
+  qthandlenewappinstance.cpp
+  qtlocalpeer.cpp
+  qtsingleapplication.cpp
+  qtsinglecoreapplication.cpp
+)
